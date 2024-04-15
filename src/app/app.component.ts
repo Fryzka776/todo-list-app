f3af279df23b405dd0f11dbfb00ba1d66e70cdde
Magdalena Kulpa <magdalena.kulpa@devqube.com>
2024-04-15 10:51:51 +0200

    fix login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { User } from './models/user.model';
 })
 export class AppComponent {
   title = 'todo-list-app';
-  constructor(){
+  constructor(private router: Router){
     if(!localStorage.getItem('users')){
       let users: User[] = [];
       const user1 = new User(1, 'exampleUser', 'examplePassword');
@@ -16,6 +17,9 @@ export class AppComponent {
       users.push(user1, user2);
   
       localStorage.setItem('users', JSON.stringify(users));
+    }
+    if(!localStorage.getItem('currentUser')){
+      router.navigate(['/login']);
     }
   }
 }
